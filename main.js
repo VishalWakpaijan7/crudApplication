5034b13ea99eab6a0c2f3d533d08e22bcd5295dd
Vishal Wakpaijan <wakpaijanvishal@gmail.com>
2021-11-28 08:11:59 +0530

    added edit button functionalty'

--- main.js
+++ main.js
@@ -1,114 +1,52 @@
-let model = document.getElementById("model");
-let modelOpen = document.getElementById("modelOpen_btn");
-let modelClose = document.getElementById("modelClose_btn");
-let modelOverlay = document.getElementById("modelOverlay");
+// Add modal
+let modal = document.getElementById("modal");
+let modalOpen = document.getElementById("modalOpen_btn");
+let modalClose = document.getElementById("modalClose_btn");
+let modalOverlay = document.getElementById("modalOverlay");
+let editModal = document.getElementById("editModalForm");
+let editModalOverlay = document.getElementById("modalOverlay");
 
-modelOpen.addEventListener("click", () => {
-  model.classList.add("open_model");
-  modelOverlay.classList.add("overlay_active");
+modalOpen.addEventListener("click", () => {
+  modal.classList.add("open_modal");
+  modalOverlay.classList.add("overlay_active");
 });
 
-modelClose.addEventListener("click", () => {
-  model.classList.remove("open_model");
-  modelOverlay.classList.remove("overlay_active");
+modalClose.addEventListener("click", () => {
+  modal.classList.remove("open_modal");
+  modalOverlay.classList.remove("overlay_active");
 });
-
-// Adding Employees
-
-function addEmployee(e) {
-  e.preventDefault();
-  let name = document.getElementById("employeeName").value;
-  let email = document.getElementById("employeeEmail").value;
-  let position = document.getElementById("position").value;
-  let id = Math.floor(Math.random() * 100 + 1);
-
-  let table = document.getElementById("data");
-
-  if ((id, name, email, position !== "--Select value--")) {
-    let index = storeData({
-      id,
-      name,
-      email,
-      position,
-    });
-    table.innerHTML += `<tr>
-    <td>${id}</td>
-    <td>${name}</td>
-    <td>${email}</td>
-    <td>${position}</td>
-    <td>
-      <button class="btn btn-delete" onclick=(removeHandler(${index})) >Remove</button>
-      <button class="btn btn-edit">Edit</button>
-      <button class="btn btn-view-profile" >View Profile</button>
-    </td>
-  </tr>`;
-  } else {
-    let pop = document.createElement("p");
-    let node = document.createTextNode("* Select Value *");
-    pop.appendChild(node);
-
-    let para = document.getElementById("pop");
-    para.appendChild(pop);
-
-    setTimeout(() => {
-      para.removeChild(pop);
-    }, 2000);
-  }
-
-  document.getElementById("employeeName").value = null;
-  document.getElementById("employeeEmail").value = null;
-  document.getElementById("position").value = `--Select value--`;
-  model.classList.remove("open_model");
-  modelOverlay.classList.remove("overlay_active");
-}
 
 function initialData() {
   let employee = localStorage.getItem("person");
   employee = JSON.parse(employee);
-
   let table = document.getElementById("data");
   if (employee) {
-    let row = "";
     let i;
+    let row = `<tr>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Position</th>
+    <th>Actions</th>
+  </tr>`;
     for (i = 0; i < employee.length; i++) {
       row += `
-      <tr>
-            <th>Employee Id</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Position</th>
-            <th>Actions</th>
-      </tr>
-      
-      <tr>
-      <td>${employee[i].id}</td>
+       <tr>
       <td>${employee[i].name}</td>
       <td>${employee[i].email}</td>
       <td>${employee[i].position}</td>
       <td>
-        <button class="btn btn-delete" onclick="removeHandler(${i})">Remove</button>
-        <button class="btn btn-edit">Edit</button>
-        <button class="btn btn-view-profile" >View Profile</button>
+        <button class="btn btn-delete"  onclick="removeHandler(${i})">Remove</button>
+        <button class="btn btn-edit edit_btn" onclick="populateEdit('${employee[i].name}','${employee[i].email}','${employee[i].position}',${i})">Edit</button>
+        <button class="btn btn-view-profile"  >View Profile</button>
       </td>
     </tr>`;
     }
     table.innerHTML = row;
-  } else {
-    table.innerHTML = `<h2>No Data</h2>`;
   }
 }
 initialData();
 
-// Remove Handler
-
-function removeHandler(index) {
-  let employee = localStorage.getItem("person");
-  employee = JSON.parse(employee);
-  employee.splice(index, 1);
-  localStorage.setItem("person", JSON.stringify(employee));
-
-  initialData();
-}
+// onclick="ViewProfile('${employee[i].name}','${employee[i].email}','${employee[i].position}',${i})"
 
 // Store Data
 
@@ -126,4 +64,103 @@ function storeData(newEmployee) {
   return person.length;
 }
 
+// Adding Employees
+
+function addEmployee() {
+  let name = document.getElementById("employeeName").value;
+  let email = document.getElementById("employeeEmail").value;
+  let position = document.getElementById("position").value;
+
+  let table = document.getElementById("data");
+
+  if ((name, email, position !== "--Select value--")) {
+    storeData({
+      name,
+      email,
+      position,
+    });
+    table.innerHTML += `
+    <tr>
+    <td>${name}</td>
+    <td>${email}</td>
+    <td>${position}</td>
+    <td>
+        <button class="btn btn-delete">Remove</button>
+        <button class="btn btn-edit" id="edit_btn">Edit</button>
+        <button class="btn btn-view-profile" >View Profile</button>
+      </td>
+  </tr>`;
+  } else {
+    let pop = document.createElement("p");
+    let node = document.createTextNode("* Select Value *");
+    pop.appendChild(node);
+
+    let para = document.getElementById("pop");
+    para.appendChild(pop);
+
+    setTimeout(() => {
+      para.removeChild(pop);
+    }, 2000);
+  }
+
+  document.getElementById("employeeName").value = null;
+  document.getElementById("employeeEmail").value = null;
+  document.getElementById("position").value = `--Select value--`;
+  modal.classList.remove("open_modal");
+  modalOverlay.classList.remove("overlay_active");
+  initialData();
+}
+
+// Remove Handler
+
+function removeHandler(index) {
+  let employee = localStorage.getItem("person");
+  employee = JSON.parse(employee);
+  employee.splice(index, 1);
+  localStorage.setItem("person", JSON.stringify(employee));
+  initialData();
+}
+
+// Edit Employee
+
+const populateEdit = (Ename, Eemail, Eposition, Eid) => {
+  editModal.classList.add("open_modal");
+  editModalOverlay.classList.add("overlay_active");
+
+  document.getElementById("editName").value = Ename;
+  document.getElementById("editEmail").value = Eemail;
+  document.getElementById("editPosition").value = Eposition;
+
+  document.getElementById("editForm").addEventListener("submit", () => {
+    let id = Eid;
+    let name = document.getElementById("editName").value;
+    let email = document.getElementById("editEmail").value;
+    let position = document.getElementById("editPosition").value;
+
+    let employee = localStorage.getItem("person");
+    employee = JSON.parse(employee);
+
+    if (employee && Array.isArray(employee)) {
+      employee[id] = { name, email, position };
+
+      localStorage.setItem("person", JSON.stringify(employee));
+    }
+
+    document.getElementById("editName").value = null;
+    document.getElementById("editEmail").value = null;
+    document.getElementById("editPosition").value = null;
+  });
+};
+
+let editedName = document.getElementById("editName").value;
+let editedEmail = document.getElementById("editEmail").value;
+let editedPosition = document.getElementById("editPosition").value;
+
 document.getElementById("employeeform").addEventListener("submit", addEmployee);
+
+document.getElementById("editModelClose").addEventListener("click", () => {
+  editModal.classList.remove("open_modal");
+  editModalOverlay.classList.remove("overlay_active");
+});
+
+//
